a61af6204bc36f765c8cd19965d6c7a856b5f664
Jorge Valdes <jorge@joval.info>
2022-02-14 11:37:38 -0600

    ct_info
    update text, reserve space to minimize status bar resizing

--- scripts/ct_info.sh
+++ scripts/ct_info.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 max=$(sysctl net.netfilter.nf_conntrack_max |awk '{print $NF}')
 now=$(sysctl net.netfilter.nf_conntrack_count |awk '{print $NF}')
-echo -n "Conn: $now/$max"
+if [ ${#max} -eq 5 ]; then
+  printf "%5d/%5d Conn" $now $max
+elif [ ${#max} -eq 6 ]
+  printf "%6d/%6d Conn" $now $max
+elif [ ${#max} -eq 7 ]
+  printf "%7d/%7d Conn" $now $max
+else
+  printf "%d/%d Conn" $now $max
+fi
