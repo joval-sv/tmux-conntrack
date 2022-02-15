d471a66ae8cce196b949deb9187da3271d97b79e
Jorge Valdes <jorge@joval.info>
2022-02-14 18:59:19 -0600

    fix helper syntax

--- scripts/ct_info.sh
+++ scripts/ct_info.sh
@@ -3,9 +3,9 @@ max=$(sysctl net.netfilter.nf_conntrack_max |awk '{print $NF}')
 now=$(sysctl net.netfilter.nf_conntrack_count |awk '{print $NF}')
 if [ ${#max} -eq 5 ]; then
   printf "%5d/%5d Conn" $now $max
-elif [ ${#max} -eq 6 ]
+elif [ ${#max} -eq 6 ]; then
   printf "%6d/%6d Conn" $now $max
-elif [ ${#max} -eq 7 ]
+elif [ ${#max} -eq 7 ]; then
   printf "%7d/%7d Conn" $now $max
 else
   printf "%d/%d Conn" $now $max
