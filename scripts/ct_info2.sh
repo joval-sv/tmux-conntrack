89fe9b86c690c317efe24baed19177406e3e06cb
Jorge Valdes <jorge@joval.info>
2022-02-14 19:20:17 -0600

    make script executable

--- scripts/ct_info2.sh
+++ scripts/ct_info2.sh
@@ -4,5 +4,6 @@ now=$(sysctl net.netfilter.nf_conntrack_count |awk '{print $NF}')
 if [ $now -eq 0 ]; then
   now=${1-1}
 fi
+# compute percent
 per=$(bc <<< "scale=10; (100*$now)/$max")
 printf "Usage: %6.1f%%" $per
