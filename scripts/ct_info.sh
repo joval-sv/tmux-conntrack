a2cd1e66aa1afd0d8956a96d6101dbc0d5176734
Jorge Valdes <jorge@joval.info>
2022-11-05 16:00:06 -0600

    Get connections on WSL

--- scripts/ct_info.sh
+++ scripts/ct_info.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+wsl=$(uname -r |egrep -c "WSL")
 max=$(sysctl net.netfilter.nf_conntrack_max |awk '{print $NF}')
-now=$(sysctl net.netfilter.nf_conntrack_count |awk '{print $NF}')
+if [ $wsl -gt 0 ]; then
+  now=$(netstat -pan |egrep -c "^(tcp|udp)")
+else
+  now=$(sysctl net.netfilter.nf_conntrack_count |awk '{print $NF}')
+fi
 if [ ${#max} -eq 5 ]; then
   printf "%5d/%5d Conn" $now $max
 elif [ ${#max} -eq 6 ]; then
